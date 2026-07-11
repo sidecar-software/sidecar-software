2d99cedc4459a0febb86aa3c84c183a3e31b13e5
Bojan Jovanovic <bojanjovanovic@Bojans-MacBook-Pro.local>
2026-07-11 07:26:36 +0200

    LSF + OSF Added

--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -7,9 +7,13 @@ import revsys from "../assets/revsys-logo.png"
 import mcfarlane from "../assets/mcfarlane-logo.webp"
 import blueweb from "../assets/blueweb-logo.svg"
 import elastic from "../assets/elastic-logo.svg"
+import opensearchFoundation from "../assets/opensearch-foundation-logo.png"
+import linuxFoundation from "../assets/linux-foundation-logo.svg"
 
 const partners = [
   { href: "https://www.elastic.co/",           logo: elastic,   alt: "Elastic",            domain: "elastic.co",            invert: false },
+  { href: "https://foundation.opensearch.org/", logo: opensearchFoundation, alt: "OpenSearch Software Foundation", domain: "foundation.opensearch.org", invert: true },
+  { href: "https://www.linuxfoundation.org/",  logo: linuxFoundation, alt: "The Linux Foundation", domain: "linuxfoundation.org", invert: true },
   { href: "https://www.kiwi.com/",             logo: kiwi,      alt: "kiwi.com",            domain: "kiwi.com",              invert: false },
   { href: "https://www.revsys.com/",           logo: revsys,    alt: "RevSys",              domain: "revsys.com",            invert: true  },
   { href: "https://www.mcfarlaneaviation.com/",logo: mcfarlane, alt: "McFarlane Aviation",  domain: "mcfarlaneaviation.com", invert: false },
@@ -22,9 +26,9 @@ export default function Partners() {
     <Appearable>
       <Helmet>
         <title>Partners | Sidecar</title>
-        <meta name="description" content="Sidecar works alongside industry leaders including Elastic, Kiwi.com, Semgrep, RevSys, and more. Trusted by companies across EMEA and North America." />
+        <meta name="description" content="Sidecar works alongside industry leaders including Elastic, the OpenSearch Software Foundation, the Linux Foundation, Kiwi.com, Semgrep, RevSys, and more. Trusted by companies across EMEA and North America." />
         <meta property="og:title" content="Partners | Sidecar" />
-        <meta property="og:description" content="Sidecar works alongside industry leaders including Elastic, Kiwi.com, Semgrep, RevSys, and more." />
+        <meta property="og:description" content="Sidecar works alongside industry leaders including Elastic, the OpenSearch Software Foundation, the Linux Foundation, Kiwi.com, Semgrep, and more." />
         <meta property="og:url" content="https://sidecar.software/#/partners" />
       </Helmet>
       <div className="partners_header">
